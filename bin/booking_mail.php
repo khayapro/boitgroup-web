681c5d1f886343959edec857537e54372b0d5a8c
khayapro <khayaprom@gmail.com>
2015-04-08 18:19:59 +0200

    added dumi, mandla, including pics & 365 pic

---
+++ bin/booking_mail.php
@@ -0,0 +1,94 @@
+<?php
+require_once 'Mail.php';
+
+
+if($_POST['fullname']!='nope'){
+    $messageBody .= 'Full Name: ' . $_POST['fullname'] . '' . "\n";
+    $messageBody .= '' . "\n";
+}
+if($_POST['company_name']!='nope'){
+    $messageBody .= 'Company Name: ' . $_POST['company_name'] . '' . "\n";
+    $messageBody .= '' . "\n";
+}else{
+    $headers = '';
+}
+
+if($_POST['company_site']!='nope'){
+    $messageBody .= 'Company Site: ' . $_POST['company_site'] . '' . "\n";
+    $messageBody .= '' . "\n";
+}
+
+if($_POST['company_email']!='nope'){
+    $messageBody .= 'Company email: ' . $_POST['company_email'] . '' . "\n";
+}
+
+if($_POST['phone_number']!='nope'){
+    $messageBody .= 'Phone Number: ' . $_POST['phone_number'] . '' . "\n";
+}
+
+if($_POST['budget']!='nope'){
+    $messageBody .= 'Your Budget: ' . $_POST['budget'] . '' . "\n";
+}
+
+if($_POST['talent_requested']!='nope'){
+    $messageBody .= 'Talent Requested: ' . $_POST['talent_requested'] . '' . "\n";
+}
+
+if($_POST['service_needed']!='nope'){
+    $messageBody .= 'Service Needed: ' . $_POST['service_needed'] . '' . "\n";
+}
+
+if($_POST['event_date']!='nope'){
+    $messageBody .= 'Event Date: ' . $_POST['event_date'] . '' . "\n";
+}
+
+if($_POST['start_time']!='nope'){
+    $messageBody .= 'Start Time: ' . $_POST['start_time'] . '' . "\n";
+}
+
+if($_POST['event_location']!='nope'){
+    $messageBody .= 'Event Venue or Location: ' . $_POST['event_location'] . '' . "\n";
+}
+
+if($_POST['event_audience']!='nope'){
+    $messageBody .= 'Event Audience Size: ' . $_POST['event_audience'] . '' . "\n";
+}
+
+if($_POST['booking_description']!='nope'){
+    $messageBody .= 'Event description: ' . $_POST['booking_description'] . '' . "\n";
+}
+
+
+
+
+/*if($_POST["stripHTML"] == 'true'){
+    $messageBody = strip_tags($messageBody);
+}*/
+
+$to = 'bookings@boittm.co.za';
+
+$header = array (
+    'From'  	=> 'bookings@boittm.co.za',
+    'To'		=> $to,
+    'Subject' 	=>  'A booking request from your site visitor:  ' . $_POST["fullname"]
+);
+
+$auth = array (
+    'auth'	=> true,
+    'host'	=> 'Mail.boittm.co.za',
+    'username' => 'bookings@boittm.co.za',
+    'password' => 'Pl@ys#2BoiT'
+);
+
+
+$smtp = Mail::factory('smtp', $auth);
+
+$mail = $smtp->send($to, $header, $messageBody);
+
+if (PEAR::isError($mail)) {
+    echo("<p>" . $mail->getMessage() . "</p>");
+} else {
+    echo("<p>Message successfully sent!</p>");
+}
+
+?>
---
+++ bin/sign_up_mail.php
@@ -0,0 +1,61 @@
+<?php
+require_once 'Mail.php';
+
+
+if($_POST['name']!='nope'){
+    $messageBody .= 'Visitor: ' . $_POST["name"] . '' . "\n";
+    $messageBody .= '' . "\n";
+}
+if($_POST['email']!='nope'){
+    $messageBody .= 'Email Address: ' . $_POST['email'] . '' . "\n";
+    $messageBody .= '' . "\n";
+}else{
+    $headers = '';
+}
+
+if($_POST['phone']!='nope'){
+    $messageBody .= 'Phone Number: ' . $_POST['phone'] . '' . "\n";
+    $messageBody .= '' . "\n";
+}
+
+if($_POST['subject']!='nope'){
+    $messageBody .= 'Subject: ' . $_POST['subject'] . '' . "\n";
+    $messageBody .= '' . "\n";
+}
+
+
+if($_POST['message']!='nope'){
+    $messageBody .= 'Message: ' . $_POST['message'] . '' . "\n";
+}
+
+/*if($_POST["stripHTML"] == 'true'){
+    $messageBody = strip_tags($messageBody);
+}*/
+
+$to = 'talent@boittm.co.za';
+
+$header = array (
+    'From'  	=> 'talent@boittm.co.za',
+    'To'		=> $to,
+    'Subject' 	=>  'A talent message from your site visitor : ' . $_POST["name"]
+);
+
+$auth = array (
+    'auth'	=> true,
+    'host'	=> 'Mail.boittm.co.za',
+    'username' => 'talent@boittm.co.za',
+    'password' => 'Pl@ys#2BoiT'
+);
+
+
+$smtp = Mail::factory('smtp', $auth);
+
+$mail = $smtp->send($to, $header, $messageBody);
+
+if (PEAR::isError($mail)) {
+    echo("<p>" . $mail->getMessage() . "</p>");
+} else {
+    echo("<p>Message successfully sent!</p>");
+}
+
+?>
